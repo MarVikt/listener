da618c868bb8bba69f24840caedf06764759d6bb
MarVikt <v.m.podporogie@gmail.com>
2022-04-08 14:58:02 +0300

    completed task #11

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const btn = document.getElementById('btn');
+const btnInCircle = document.getElementById('e_btn');
+const square = document.getElementById('square');
+const circle = document.getElementById('circle');
+const range = document.getElementById('range');
+let inputText = document.getElementById('text');
 
+const changeColorSquare = function () {
+  square.style.background = inputText.value;
+};
 
+const changeCircle = function (event) {
+  console.log(event.target.value);
+  circle.style.width = event.target.value + '%';
+  circle.style.height = event.target.value + '%';
+};
 
-
-
+btn.addEventListener('click',changeColorSquare);
+btnInCircle.style.display = 'none';
+range.addEventListener('change',changeCircle);
